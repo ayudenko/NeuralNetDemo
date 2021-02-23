fe9d59655525ff08f94e9d5b129a40bd7de12571
Andrey Yudenko <ayudenko@gmail.com>
2021-02-23 23:36:19 +0300

    Draw marks on canvas

---
+++ NeuralNetDemo/Client/Entities/Mark.cs
@@ -0,0 +1,48 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeuralNetDemo.Client.Entities
+{
+    public class Mark
+    {
+
+        private readonly double _x;
+        private readonly double _y;
+        private string _color;
+        private Canvas2DContext _context;
+
+        public Mark(double x, double y, Canvas2DContext context)
+        {
+            (_x, _y, _context) = (x, y, context);
+        }
+
+        public void SetGreen()
+        {
+            _color = "Green";
+        }
+
+        public void SetRed()
+        {
+            _color = "Red";
+        }
+
+        public void SetWhite()
+        {
+            _color = "White";
+        }
+
+        public async void Draw()
+        {
+            await _context.BeginPathAsync();
+            await _context.SetFillStyleAsync(_color);
+            await _context.SetStrokeStyleAsync("Black");
+            await _context.ArcAsync(_x, _y, 5, 0, Math.PI * 2);
+            await _context.FillAsync();
+            await _context.StrokeAsync();
+        }
+
+    }
+}
---
+++ NeuralNetDemo/Client/Pages/Index.razor.cs
@@ -0,0 +1,55 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas.Canvas2D;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using NeuralNetDemo.Client.Entities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NeuralNetDemo.Client.Pages
+{
+    public partial class Index
+    {
+
+        private Canvas2DContext _context;
+        private ElementReference _divCanvas;
+
+        protected BECanvasComponent _canvasReference;
+
+        private List<Mark> marks = new List<Mark>();
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            _context = await _canvasReference.CreateCanvas2DAsync();
+        }
+
+        private async void OnClick(MouseEventArgs eventArgs)
+        {
+            var data = await jsRuntime.InvokeAsync<BoundingClientRect>("MyDOMGetBoundingClientRect", (object)_divCanvas);
+            double mouseX = (int)(eventArgs.ClientX - data.Left);
+            double mouseY = (int)(eventArgs.ClientY - data.Top);
+
+            var mark = new Mark(mouseX, mouseY, _context);
+            mark.Draw();
+            mark.SetWhite();
+            marks.Add(mark);
+        }
+
+        public class BoundingClientRect
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public double Top { get; set; }
+            public double Right { get; set; }
+            public double Bottom { get; set; }
+            public double Left { get; set; }
+        }
+
+    }
+}
